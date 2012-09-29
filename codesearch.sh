e55deed3155e23c1a70dfa92a824c48db2035902
Ricardo Montania <ricardo@linuxafundo.com.br>
2012-09-29 11:34:44 -0300

    License correction in codesearch.sh

--- codesearch.sh
+++ codesearch.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 # codesearch.sh
 
-# This program is free software. You can redistribute it and/or modify
-# is under the terms of the GNU Gerenal Public License as published by
-# the Free Software Foundation, either version 2 of the License, or
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
 # any later version.
-#
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
 # Credits: Ricardo Montania - ricardo [at] linuxafundo.com.br
 
 
