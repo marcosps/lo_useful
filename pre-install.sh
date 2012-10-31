fa6439750622e34f98671e73fff2a66cac37c7fa
Ricardo Montania <ricardo@linuxafundo.com.br>
2012-10-31 13:44:15 -0200

    pre-install.sh done

--- pre-install.sh
+++ pre-install.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+
+
+
+final()
+{
+    echo " "; echo " "; echo " ";
+    echo "Principais dependencias instaladas!"
+}
+
+##
+## Script begin
+##
 clear
 echo " Instalacao dos pre-requisitos para compilar o LO"
 echo " "
 echo "1. Debian/Ubuntu"
-echo "2. Fedora"
+echo "2. Fedora"; echo " "
 read -p "Escolha sua distro: " distro
 
 if [ "$distro" == "1" ]; then
@@ -13,13 +25,14 @@ if [ "$distro" == "1" ]; then
     sudo apt-get update
     sudo apt-get build-dep libreoffice
     sudo apt-get install git-core libgnomeui-dev gawk junit4 doxygen
+    final
 elif [ "$distro" == "2" ]; then
     echo "Instalacao dos pre-requisitos em ambiente Fedora"
     echo " "
     sudo yum update
     sudo yum-builddep libreoffice
     sudo yum install git libgnomeui-devel gawk junit doxygen
+    final
+else
+    echo "Desculpe, opcao invalida!"
 fi
-
-clear
-echo "Dependencias principais concluidas!"
