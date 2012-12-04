0e440f79e70a8261390f540eed944f8bc00fac41
alexvicenzi <vicenzi.alexandre@gmail.com>
2012-12-04 14:52:11 -0200

    Added openSUSE dep install

--- pre-install.sh
+++ pre-install.sh
@@ -9,12 +9,6 @@
 ###              install and clone the source (standard), or just dependencies
 ###              install.
 
-### Functions:
-### bash pre-install.sh                 - Dep install and clone to $HOME/libo folder
-### bash pre-install.sh /some/folder    - Dep install and clone in the received folder
-### bash pre-install.sh --no-clone      - Only dep install, don't clone
-### bash pre-install.sh --help          - Show help message
-
 ### LICENSE
 ### This program is free software: you can redistribute it and/or modify
 ### it under the terms of the GNU General Public License as published by
@@ -35,149 +29,218 @@
 ### - Jose Guilherme Vanz ....(guilherme.sft [at] gmail.com)
 ###
 
-
-
-
-###
-### Function section
-###
-
-###
-### Usage and syntax
-### 
 usageSyntax()
 {
     echo " "
-    echo "Usage:"; echo " ";
-    echo "bash pre-install.sh [option]"; echo " "
+    echo "Usage:"
+    echo " "
+    echo "bash pre-install.sh [option]"
+    echo " "
 
-    echo "Options:"; echo " "
-    echo " /some/folder/    - Dep install and clone in /some/folder/"
-    echo " --no-clone       - Only dep install, don't clone"
-    echo " --help           - Show this help message"; echo " ";
-    echo "If no parameters was informed, whill be installed the deps"
-    echo " and git clone in $HOME/libo/ folder"
+    echo "Options:"
+    echo " "
+    echo " --dir [/some/folder/]    - Dep install and clone in /some/folder/"
+    echo "                          - If no dir was informed, whill be installed" 
+    echo "                            the deps and git clone in $HOME/libo/ folder"
+    echo " --no-clone               - Only dep install, don't clone"
+    echo " --ccache                 - Install ccache. It speeds up recompilation by"
+    echo "                            caching previous compilations and detecting when"
+    echo "                            the same compilation is being done again"
+    echo " --help                   - Show this help message"
+    echo " "
 }
 
-###
-### Function that works according with the parameter received
-###
-actionByParameter()
+gitClone()
 {
-    ###
-    ### If the parameter is --no-clone, exit
-    ###
-    if [ "$param" == "--no-clone" ]; then
+    if $noclone; then
         exit
     fi
-
-    ###
-    ### If the parameter is empty, clone for the $HOME/libo folder
-    ###
-    if [ "$param" == "" ]; then
+	
+    if [ $clonedir == "" ]; then
         cd $HOME
-        git clone git://anongit.freedesktop.org/libreoffice/core libo && echo " " && "Success!"
+        #git clone git://anongit.freedesktop.org/libreoffice/core libo && echo " " && "Success!"
         exit
     fi
 
-    ###
-    ### If the parameter is a valid and writable folder, clone there
-    ###
-    if [ -d "$param" ]; then
-        cd "$param"
-        git clone git://anongit.freedesktop.org/libreoffice/core libo && echo " " && "Success!"
+    # Uses the directory informed.
+    if [ -d "$clonedir" ]; then
+        cd "$clonedir"
+        #git clone git://anongit.freedesktop.org/libreoffice/core libo && echo " " && "Success!"
         exit
-
-    ###
-    ### If not, try create and then to clone
-    ###
     else
-        mkdir -p $param
-
-        ###
-        ### Verify again if the folder exists, and if not
-        ###  show an error message and exit.
-        ###
-        if [ -d "$param" ]; then
-            cd $param
-            git clone git://anongit.freedesktop.org/libreoffice/core libo && echo " " && "Success!"
+        # If not exists, try create.
+        mkdir -p $clonedir
+        if [ -d "$clonedir" ]; then
+            cd "$clonedir"
+            #git clone git://anongit.freedesktop.org/libreoffice/core libo && echo " " && "Success!"
             exit
         else
-            echo "Unable to create '$param' folder"
+            echo "Unable to create '$clonedir' folder"
             exit
         fi
     fi
 }
 
-
-
-###
-### Script Begin
-###
-
-###
-### Receive the first parameter informed
-###
-param="$1"
-
-###
-### if there is two parameters or more, exit. 
-###
-if [ "$2" ]; then
-    echo "Syntax error!";
-    usageSyntax
-    exit
-
-###
-### Or if the parameter is --help, show the help message
-###
-elif [ "$param" == "--help" ]; then
-    usageSyntax
-    exit
-fi
-
-
-
-
-###
-### Begin of the interactive section
-###
-clear
-echo " Script for dependencies installation to compile LibreOffice."
-
-###
-### Show the destiny folder according to the case
-###
-if [ "$param" == "" ]; then
-    echo " Path (absolute) for the folder where will be cloned the libo repository: '$HOME'"; echo " "
-elif [ "$param" == "--no-clone" ]; then
-    echo " Chosen option for don't clone!"; echo " "
-else
-    echo " Path (absolute) for the folder where will be cloned the libo repository: '$param'"; echo " "
-fi
-
-echo "1. Debian/Ubuntu"
-echo "2. Fedora"; echo " "
-read -p "Choice your distro: " distro
-
-if [ "$distro" == "1" ]; then
+debianInstall()
+{
     echo "Dep install for Debian/Ubuntu"
     echo " "
     sudo apt-get update
+
+    if $inccache; then
+        sudo apt-get install ccache
+    fi
+
     sudo apt-get build-dep libreoffice -y
     sudo apt-get install git-core libgnomeui-dev gawk junit4 doxygen -y
+}
 
-    actionByParameter
-
-elif [ "$distro" == "2" ]; then
+fedoraInstall()
+{
     echo "Dep install for Fedora"
     echo " "
     sudo yum update
+	
+    if $inccache; then
+        sudo yum install ccache
+    fi
+
     sudo yum-builddep libreoffice -y
     sudo yum install git libgnomeui-devel gawk junit doxygen -y
-    
-    actionByParameter
+}
 
-else
-    echo "Sorry, invalid option!"
-fi
+suseInstall()
+{
+    echo " "
+    echo "1. 11.4"
+    echo "2. 12.1"
+    echo "3. 12.2"
+    echo "4. 12.3"
+    echo " "
+    read -p "Select your SUSE version: " version
+
+    case $version in
+        1) vername="11.4";;
+        2) vername="12.1";;
+        3) vername="12.2";;
+        4) vername="12.3";;
+        *) echo "Sorry, invalid option!"; exit;;
+    esac
+
+    echo " "
+    echo "1. 32 bits"
+    echo "2. 64 bits"
+    echo " "
+    read -p "Select your system type: " systype
+
+    case $systype in
+        1) sysname="32 bits";;
+        2) sysname="64 bits";;
+        *) echo "Sorry, invalid option!"; exit;;
+    esac
+    
+    echo " "
+    echo "Dep install for openSUSE" $vername $sysname
+    echo "If you receive a question to answer [yes or no], please, read carrefully."
+    echo "Maybe root's password will be asked more than once."
+    echo " "	
+
+    sudo zypper refresh
+    sudo zypper update
+    sudo zypper in java-1_7_0-openjdk-devel # gcj is installed by default but it does not work reasonably.
+    
+    if $inccache; then
+        sudo zypper in ccache
+    fi
+
+    if [$systype == 1]; then
+        sudo zypper in krb5-devel-32bits
+    else
+        sudo zypper in krb5-devel
+    fi	
+	
+    sudo zypper mr --enable "openSUSE-$vername-Source" # Enable te repo to download the source.
+    sudo zypper si -d libreoffice # For OpenSUSE 11.4+ (was OpenOffice_org-bootstrap)
+    sudo zypper in git libgnomeui-devel gawk junit doxygen
+}
+
+showDestination()
+{
+    if [ "$clonedir" == "" ]; then
+        echo "Path (absolute) for the folder where will be cloned the libo repository: '$HOME'"; echo " "
+    elif [ "$clonedir" == "--no-clone" ]; then
+        echo "Chosen option for don't clone!"; echo " "
+    else
+        echo "Path (absolute) for the folder where will be cloned the libo repository: '$clonedir'"; echo " "
+    fi
+}
+
+distroChoice()
+{
+    echo "1. Debian/Ubuntu"
+    echo "2. Fedora"
+    echo "3. openSUSE"
+    echo "4. Other linux"
+    echo " "
+
+    read -p "Choice your distro: " distro
+
+    case $distro in
+        1) debianInstall;;
+        2) fedoraInstall;;
+        3) suseInstall;;
+        4) echo "Please, follow the instructions in \"http://www.libreoffice.org/developers-2\" to configure you system or contact us."; exit;;
+        *) echo "Sorry, invalid option!"; exit;;
+    esac
+}
+
+cloneSyntaxError()
+{
+    echo "Syntax error. Cannot use --no-clone and --dir together."
+    usageSyntax
+    exit
+}
+
+inccache=false
+noclone=false
+clonedir=""
+
+###
+### Check input parameters.
+###
+while [ $# -ne 0 ]
+do
+    case $1 in
+        "--help") usageSyntax; exit;;
+        "--ccache") inccache=true;;
+        "--no-clone")
+            if [ "$clonedir" != "" ]; then
+                cloneSyntaxError
+            fi 
+            noclone=true;;
+        "--dir")		
+            if $noclone; then
+                cloneSyntaxError
+            fi
+
+            # Get dir name.
+            shift
+            clonedir=$1
+
+            dr=${clonedir:0:1}
+            # Need a name before the next parameter.
+            if [ "$dr" == "-" ]; then
+                echo "Syntax error. Invalid directory \"$clonedir\"."; exit
+            fi
+            ;;
+        *) echo "Syntax error. Invalid parameter \"$1\"."; usageSyntax; exit;;
+    esac
+    # Next parameter.
+    shift
+done
+
+clear
+echo "Script for dependencies installation to compile LibreOffice."
+showDestination;
+distroChoice;
+gitClone;
