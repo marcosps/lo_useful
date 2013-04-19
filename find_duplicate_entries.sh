bb15c0746354d5860e77cc6d2839601c4742490f
Marcos Paulo de Souza <marcos.souza.org@gmail.com>
2013-04-18 23:43:03 -0300

    Shell para encontrar includes e namespaces duplos

---
+++ find_duplicate_entries.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+ 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# Author: Marcos Paulo de Souza <marcos.souza.org@gmail.com>
+# This script will help to find duplicate includes and using declarations
+
+for i in `find . -type f \( -name "*.cxx" -o -name "*.h" \) -print` 
+do
+	for inc in `egrep "#include.*" $i | uniq | awk '{ print $2 }' `
+	do
+		num=$(grep $inc $i | wc -l)
+		if [ "$num" -gt "1" ]
+		then
+			echo "$i: Header $inc is duplicated"
+		fi
+	done
+
+	for use in `egrep "using namespace" $i | uniq | awk '{ print $3 }' `
+	do
+		num=$(grep $use $i | wc -l)
+		if [ "$num" -gt "1" ]
+		then
+			echo "$i: Namespace $use is duplicated"
+		fi
+	done
+done
